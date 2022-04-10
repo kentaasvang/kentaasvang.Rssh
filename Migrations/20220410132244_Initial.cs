396908e081bc90bb5c3cf1269bcdc85ce07d515d
Kent Martin Åsvang <kent@asvang.no>
2022-04-10 15:38:58 +0200

    initial migrations on sql db

---
+++ Migrations/20220410132244_Initial.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kent.Cli.Rssh.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConnectionDetails",
+                columns: table => new
+                {
+                    ConnectionDetailId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Ip = table.Column<string>(type: "TEXT", nullable: true),
+                    Password = table.Column<string>(type: "TEXT", nullable: true),
+                    Username = table.Column<string>(type: "TEXT", nullable: true),
+                    GroupId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConnectionDetails", x => x.ConnectionDetailId);
+                    table.ForeignKey(
+                        name: "FK_ConnectionDetails_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConnectionDetails_GroupId",
+                table: "ConnectionDetails",
+                column: "GroupId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConnectionDetails");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+        }
+    }
+}
