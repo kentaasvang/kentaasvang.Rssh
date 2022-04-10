15d6b4017b2c0bb2d62aa114a05b14f5912eca1d
Kent Martin Åsvang <kent@asvang.no>
2022-04-10 15:42:16 +0200

    removed some comments

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.CommandLine;
+﻿using System;
+using System.CommandLine;
 using System.Threading.Tasks;
 using Kent.Cli.Rssh.Extensions;
 
@@ -8,39 +9,19 @@ internal class Program
 {
     internal static async Task<int> Main(string[] args)
     {
-        RootCommand rootCommand = new();
-
-        // API 
-        // Usage
-        // rssh <server>
-        // rssh <group> <server>
-
-        // adding new groups or servers
-        // rssh add <server> (prompts username and password and IP) 
-        // rssh add <group> <server> (prompts username and password and IP) 
-
-        // list out groups and servers
-        //  >>> rssh list
-        //  Groups:
-        //      Ebs
-        //          stage
-        //          develop
-        //          prod
-        //      Home
-        //          main
-        //          raspberry  
-
-        // remove groups or single server
-        // rssh remove <group> -> removes group totally with all servers belonging to that group
-        // rssh remove <group> <server> -> removes only this server 
-        // rssh remove <server> -> removes only this server
-
         // TODO: extract configuration
-        rootCommand.Description = "store ssh credentials for clients";
-        rootCommand.Name = "rssh";
+        // rssh <group> <server>
+        // rssh <server>
+        // Usage
+        // API 
+        RootCommand rootCommand = new()
+        {
+            Description = "store ssh credentials for clients",
+            Name = "rssh"
+        };
 
         rootCommand.ConfigureCommands();
-
+        
         await rootCommand.InvokeAsync(args);
 
         return 0;
